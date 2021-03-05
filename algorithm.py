6628c088cd4eb33a4f33ca19fd95f4d7a34285e5
LeeJeongMin <ekdmsdlfma@gmail.com>
2021-03-05 18:09:31 +0900

    변수 이름 변경

--- algorithm.py
+++ algorithm.py
@@ -5,24 +5,28 @@
 #incline[2] = incline of the left line on the warped image
 #incline[3] = incline of the right line on the warped image
 
-#left_x = left x coordinates on wapred image
-#right_x = right x coordinates on wapred image
-#left_y = left y coordinates on wapred image
-#right_y = right y coordinates on wapred image
+#warpLeft = left x coordinates on wapred image
+#warpRight = right x coordinates on wapred image
 
-#left_or_x = left x coordinates on original image
-#right_or_x = right x coordinates on original image
-#left_or_y = left y coordinates on original image
-#right_or_y = right y coordinates on original image
+#originalLeft_x = left x coordinates on original image
+#originalRight_x = right x coordinates on original image
+#originalLeft_y = left y coordinates on original image
+#originalRight_y = right y coordinates on original image
 
-def algorithm_custom(incline,draw_left,draw_right,left_x,right_x,left_or_x,right_or_x,left_or_y,right_or_y):
+def algorithm_custom(incline, draw_left, draw_right, warpLeft, warpRight, originalLeft_x, originalRight_x, originalLeft_y, originalRight_y):
+    class Point():
+        def __init__(self,x,y):
+            self.x = x
+            self.y = y
+    
+    leftPoints = list(map(lambda x,y : Point(x,y),(originalLeft_x,originalLeft_y)))
+    rightPoints = list(map(lambda x,y : Point(x,y),(originalRight_x,originalRight_y)))
 
-#Detection left or right line existance
     left = True
     right = True
 
     try:
-        if (left_x[0] > 180  or incline[0] == 0):
+        if (warpLeft[0] > 180  or incline[0] == 0):
             print('no left')
             left = False
     except:
@@ -30,20 +34,19 @@ def algorithm_custom(incline,draw_left,draw_right,left_x,right_x,left_or_x,right
         left = False
 
     try:
-        if (right_x[0] < 180 or incline[1] == 0):
+        if (warpRight[0] < 180 or incline[1] == 0):
             print('no right')
             right = False
     except:
         print('no right')
         right = False
-############################################
 
 #start code from here
     steer = 1500 # min : 500 , max : 2500
-    speed = 1000 # min : -5000 , max : 5000 , middle : 0
+    speed = 3000 # min : -5000 , max : 5000 , middle : 0
 
     try:
-        mean = (left_or_x[0]+right_or_x[0])/2
+        mean = (originalLeft_x[0]+originalRight_x[0])/2
     except:
         pass
 
@@ -57,7 +60,7 @@ def algorithm_custom(incline,draw_left,draw_right,left_x,right_x,left_or_x,right
             
     elif left and not right:
         steer = 1800
-        if((left_or_y[0]+left_or_y[-1])/2 > 150):
+        if((originalLeft_y[0]+originalLeft_y[-1])/2 > 150):
             steer = 1900
 
         
