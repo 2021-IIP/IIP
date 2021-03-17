6675f2b2ed6bca1dcb2eb1335bd2237668fe17cd
byj317 <80030258+byj317@users.noreply.github.com>
2021-03-17 23:19:06 +0900

    :D

--- algorithm.py
+++ algorithm.py
@@ -10,35 +10,38 @@ class Algorithm():
         return self.parameter.originalLeft_x[index] + self.parameter.originalRight_x[index]
 
     def twoPointIncline(self):
-        direction = (self.getSumOfY(0) + self.getSumOfY(-1)) / (self.getSumOfX(0) + self.getSumOfX(-1))
-        return direction
+        direction = (self.getSumOfX(0) + self.getSumOfX(-1)) / (self.getSumOfX(0) + self.getSumOfX(-1))
+        return 1500 * direction, self.parameter.speed
 
     def threePointIncline(self):
-        direction = (self.getSumOfY(0) + self.getSumOfY(-5) + self.getSumOfY(-1)) / (self.getSumOfX(0) + self.getSumOfY(-5) + self.getSumOfX(-1))
-        return direction
+        direction = (self.getSumOfX(0) + self.getSumOfX(0) + self.getSumOfX(-1)) / (self.getSumOfY(0) + self.getSumOfY(0) + self.getSumOfY(-1))
+        return 1500 * direction , self.parameter.speed
 
     def sumPoint(self):
-        direction = (sum(self.parameter.originalLeft_y)/len(self.parameter.originalLeft_y) + sum(self.parameter.originalRight_y)/len(self.parameter.originalRight_y)) / (sum(self.parameter.originalLeft_x)/len(self.parameter.originalLeft_x) + (sum(self.parameter.originalRight_x)/len(self.parameter.originalRight_x)))
-        return direction
+        right_x = sum(self.parameter.originalRight_x)/len(self.parameter.originalRight_x)
+        left_x = sum(self.parameter.originalLeft_x)/len(self.parameter.originalLeft_x)
+        left_y = sum(self.parameter.originalLeft_y)/len(self.parameter.originalLeft_y)
+        right_y = sum(self.parameter.originalRight_y)/len(self.parameter.originalRight_y)
+        direction = (left_x + right_x) / (left_y + right_y)
+        if (self.parameter.incline[0] * self.parameter.incline[1]) > 0:
+            self.parameter.speed *= 0.25
+        return 1500 * direction , self.parameter.speed
     
-    def inclineControl(self):
-        left = self.parameter.incline[0]
-        right = self.parameter.incline[1]
-        if (left * right) < 0:
-            return -(left + right)
-        else:
-            return -(left * right)
-    
-    def meanTwoPointIncline(self):
-        direction = (self.getSumOfY(0) + self.getSumOfY(1)) / (Self.getSumOfX(0) + self.getSumOfX(1))
-        return direction
+    def caseByCase(self):
+        right_x = sum(self.parameter.originalRight_x)/len(self.parameter.originalRight_x)
+        left_x = sum(self.parameter.originalLeft_x)/len(self.parameter.originalLeft_x)
+        left_y = sum(self.parameter.originalLeft_y)/len(self.parameter.originalLeft_y)
+        right_y = sum(self.parameter.originalRight_y)/len(self.parameter.originalRight_y)
+        if self.sumPoint()/1500:
+            pass
+        return 1500 * direction , self.parameter.speed
 
 def algorithm_custom(incline, draw_left, draw_right, warpLeft, warpRight, originalLeft_x, originalRight_x, originalLeft_y, originalRight_y):
     steer = 1500 # min : 500 , max : 2500
-    speed = 1500 # min : -5000 , max : 5000 , middle : 0
+    # min : -5000 , max : 5000 , middle : 0
 
     class Parameter():
-        def __init__(self):
+        def __init__(self,speed):
             self.incline = incline
             self.warpLeft = warpLeft
             self.warpRight = warpRight
@@ -46,11 +49,7 @@ def algorithm_custom(incline, draw_left, draw_right, warpLeft, warpRight, origin
             self.originalRight_x = originalRight_x
             self.originalLeft_y = originalLeft_y
             self.originalRight_y = originalRight_y
+            self.speed = speed
 
-    parameter = Parameter()
-    direction = Algorithm(parameter)
-
-    direction = direction.sumPoint()
-    steer = 1500 * direction
-
-    return steer , speed
+    algorithm = Algorithm(Parameter(5000))
+    return algorithm.sumPoint()
